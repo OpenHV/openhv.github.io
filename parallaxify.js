fdd0b153865e43706e62d9fc084fba485be53837
Carson <carsonburke22@gmail.com>
2024-04-19 11:22:00 -0700

    Squashed commit of the following:
    commit dc6e1d8eb5d29f387cf7a59942f2c63db87725c8
    Author: Carson <carsonburke22@gmail.com>
    Date:   Fri Apr 19 11:19:02 2024 -0700
    
        re-added correct chocolatey icon, replace web icon
    
    commit d96c122c981c7b2444c4bf1579378eb995f27b3f
    Author: Carson <carsonburke22@gmail.com>
    Date:   Thu Apr 18 20:41:41 2024 -0700
    
        proper path for engine link
    
    commit 55a1e8e49359b6b7233698a378924584137de6b9
    Author: Carson <carsonburke22@gmail.com>
    Date:   Thu Apr 18 20:39:08 2024 -0700
    
        custom scrollbar
    
    commit de28aed7fb3fd838bfe34899147cfde5da1ee6bb
    Author: Carson <carsonburke22@gmail.com>
    Date:   Thu Apr 18 20:32:11 2024 -0700
    
        Delete combat.mp4
    
    commit 88e3898445ff51e54fde48d8e9e95ce370a6822d
    Author: Carson <carsonburke22@gmail.com>
    Date:   Thu Apr 18 20:28:30 2024 -0700
    
        few more keywords for games page
    
    commit f21117d2c954dd1509c2bcfa6046222a936491de
    Author: Carson <carsonburke22@gmail.com>
    Date:   Thu Apr 18 20:24:35 2024 -0700
    
        hollistic style refactor
    
        -change font to poppins
        -increase some font sizes
        -stylize overview
        -distinct overview images
        -favicon and fonts on games page
        -banner
    
    commit 6c7bb9b9817b3b975a02a015a945ad5015660d07
    Author: Carson <carsonburke22@gmail.com>
    Date:   Thu Apr 18 18:35:09 2024 -0700
    
        added parallax to hero background
    
    commit 21d6940b59a43070e2257152be7a1a3c8fff1b34
    Author: Carson <carsonburke22@gmail.com>
    Date:   Thu Apr 18 18:29:59 2024 -0700
    
        add favicon of shrunk icon
    
    commit 900bb86d98590cfca00417ab9252fa37222098b1
    Merge: 78e70f6 640dfb6
    Author: Carson <carsonburke22@gmail.com>
    Date:   Thu Apr 18 18:22:09 2024 -0700
    
        Merge branch 'main' of https://github.com/CarsonBurke/openhv.github.io
    
    commit 78e70f63b38ad5f3482469aeb623fedf17443020
    Author: Carson <carsonburke22@gmail.com>
    Date:   Sun Apr 7 01:33:31 2024 -0700
    
        minor text formatting adjustments
    
    commit bb275062b56407157aa7bb3360c6eee95ed36a34
    Author: Carson <carsonburke22@gmail.com>
    Date:   Sun Apr 7 01:17:52 2024 -0700
    
        refactor layout and styling
    
        - remove downloads and links page
        - modify palette
        - entirely redesign homepage
        - modify styling and formatting of lobby browser

---
+++ parallaxify.js
@@ -0,0 +1,49 @@
+// Unlicensed by Carson Burke @MarvinTMB
+
+// Not exactly the most efficient parallax implementation, but it should work fine enough
+// Use (css) classes parallaxify for <img /> tags, and parallaxifyBg for background images 
+
+window.addEventListener("load", parallax)
+window.addEventListener("scroll", parallax)
+
+function parallax() {
+
+    function getPosition(item) {
+
+        let top = (item.getBoundingClientRect().top / window.innerHeight * 100).toFixed(2)
+
+        return top
+    }
+
+    let itemsToParallax = document.getElementsByClassName("parallaxify")
+
+    for (let item of itemsToParallax) {
+
+        if (!item.dataset.speedMultiplier) {
+
+            item.dataset.speedMultiplier = "0.7"
+        }
+
+        item.style.transform = "translate3d(0, " + getPosition(item) * item.dataset.speedMultiplier + "px, 0)"
+    }
+
+    let imagesToParallax = document.getElementsByClassName("parallaxifyBg")
+
+    for (let item of imagesToParallax) {
+
+        function getPosition(item) {
+
+            let top = (item.getBoundingClientRect().top / window.innerHeight * 100).toFixed(2)
+
+            return top
+        }
+
+        if (!item.dataset.speedMultiplier) {
+
+            item.dataset.speedMultiplier = "3"
+        }
+
+        item.style.backgroundAttachment = "fixed"
+        item.style.backgroundPositionY = getPosition(item) * item.dataset.speedMultiplier + "px"
+    }
+}
